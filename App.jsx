82a0235e3ef1441808545f4e08c6f7ae9018e417
elmahdi <elmahdi.rammach@gmail.com>
2022-05-16 10:50:47 +0100

    version stable

--- App.jsx
+++ App.jsx
@@ -1,82 +1,112 @@
-import React ,{useState,useEffect} from 'react';
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-// import { Appbar } from 'react-native-paper';
-// import { Calendar, Agenda } from 'react-native-calendars';
-import CalendarStrip from "react-native-calendar-strip";
-import * as Calendar from "expo-calendar";
-import moment from "moment";
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
-  const [CurrentDate, setCurrentDate] = useState(new Date());
-  const datesWhitelist = [
-    {
-      start: moment(),
-      end: moment().add(365, "days"), // total 4 days enabled
-    },
-  ];
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy)
+  }
+
   return (
-    <>
-      <CalendarStrip
-        calendarAnimation={{ type: "sequence", duration: 30 }}
-        daySelectionAnimation={{
-          type: "background",
-          duration: 200,
+    <View style={styles.container}>
+      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
         }}
-        style={{
-          height: 150,
-          marginTop: 20,
-          paddingTop: 20,
-          paddingBottom: 20,
-        }}
-        calendarHeaderStyle={{ color: "#000000" }}
-        dateNumberStyle={{ color: "#000000", paddingTop: 10 }}
-        dateNameStyle={{ color: "#BBBBBB" }}
-        highlightDateNumberStyle={{
-          color: "#fff",
-          backgroundColor: "#2E66E7",
-          marginTop: 10,
-          height: 35,
-          width: 35,
-          textAlign: "center",
-          borderRadius: 17.5,
-          overflow: "hidden",
-          paddingTop: 6,
-          fontWeight: "400",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        highlightDateNameStyle={{ color: "#2E66E7" }}
-        disabledDateNameStyle={{ color: "grey" }}
-        disabledDateNumberStyle={{ color: "grey", paddingTop: 10 }}
-        datesWhitelist={datesWhitelist}
-        // iconLeft={require('../../../assets/left-arrow.png')}
-        // iconRight={require('../../../assets/right-arrow.png')}
-        iconContainer={{ flex: 0.1 }}
-        minDate={moment()}
-        maxDate={moment().add(10, "days")}
-        datesBlacklist={(date) => {
-          return date.isoWeekday() === 6 || date.isoWeekday() === 7;
-        }}
-        // selectedDate={currentDate}
-        onDateSelected={(date) => {
-          const selectedDate = `${moment(date).format('YYYY')}-${moment(
-            date
-          ).format('MM')}-${moment(date).format('DD')}`;
-          // updateCurrentTask(selectedDate);
-          setCurrentDate(selectedDate);
-          <text>{selectedDate}</text>
-        }}
-      />
-    </>
+        keyboardShouldPersistTaps='handled'
+      >
+
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
+                  <Task text={item} /> 
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+        
+      </ScrollView>
+
+      {/* Write a task */}
+      {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+      
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
 });
